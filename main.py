b482d838023b74f6400cdbaf13239753972fab6d
F1End <horvirich@gmail.com>
2026-04-18 12:28:35 +0200

    Fixing indentation

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
     args = parser.parse_args()
     logger = setup_logger()
     
-	logger.info(f"Running command: {args.command}")
+    logger.info(f"Running command: {args.command}")
     
     exit_code, stdout, stderr = run_command(args.command)
 
